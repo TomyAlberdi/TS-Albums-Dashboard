a115443f8b35874c574e477cb938b754b5f009c3
Tomás Alberdi <66546046+TomyAlberdi@users.noreply.github.com>
2025-05-17 20:30:23 -0300

    Started menu drawer implementation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,40 @@
-const Navbar = () => {
+import {
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { Button } from "@/components/ui/button";
+import { Menu } from "lucide-react";
 
+const Navbar = () => {
   return (
     <nav className="flex justify-between items-center shadow-lg bg-zinc-900">
-      navbar
+      <Drawer>
+        <DrawerTrigger asChild>
+          <Button className="ml-3 cursor-pointer" variant={"secondary"}>
+            <Menu />
+              Change Time Period
+          </Button>
+        </DrawerTrigger>
+        <DrawerContent>
+          <div className="mx-auto w-full max-w-lg">
+            <DrawerHeader>
+              <DrawerTitle>Change Time Period</DrawerTitle>
+              <DrawerDescription>
+                Update the Albums List to show the data for the selected time
+                period.
+              </DrawerDescription>
+            </DrawerHeader>
+          </div>
+        </DrawerContent>
+      </Drawer>
       <div className="p-3 cursor-pointer hover:bg-zinc-800 ease-in duration-200">
         <img src="/favicon.ico" alt="logo" className="w-[7vh] h-[7vh]" />
       </div>
     </nav>
-  )
-}
-export default Navbar
+  );
+};
+export default Navbar;
