135e81b8f660c91f7866acb51de7d0ce3129e7fd
Tomás Alberdi <66546046+TomyAlberdi@users.noreply.github.com>
2025-05-19 00:28:41 -0300

    Added decorative medals

--- src/components/AlbumCard.tsx
+++ src/components/AlbumCard.tsx
@@ -7,6 +7,9 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 
+// <a href="https://www.flaticon.com/free-icons/medal" />
+// <a href="https://www.flaticon.com/free-icons/second-place" />
+
 interface AlbumCardProps {
   album: PartialAlbum;
 }
@@ -23,14 +26,44 @@ const AlbumCard = ({ album }: AlbumCardProps) => {
   }, [album]);
 
   return (
-    <Card className="cursor-pointer p-2 gap-0 min-w-[150px] w-[48%] md:w-[13.4%] min-h-[175px] h-[27vh] md:h-[30vh] mb-[4%] md:mb-[1.03%] rounded-md bg-black/30 md:hover:-translate-y-3 md:hover:scale-100 transition-transform duration-300 ease-in-out shadow-lg z-20 flex flex-col justify-start items-center">
+    <Card className="relative cursor-pointer p-2 gap-0 min-w-[150px] w-[48%] md:w-[13.4%] min-h-[175px] h-[27vh] md:h-[30vh] mb-[4%] md:mb-[1.03%] rounded-md bg-black/30 md:hover:-translate-y-3 md:hover:scale-100 transition-transform duration-300 ease-in-out shadow-lg z-20 flex flex-col justify-start items-center">
+      <div
+        className={
+          "absolute w-1/6 aspect-square top-2 right-1 " +
+          (album["@attr"].rank === "1" ||
+          album["@attr"].rank === "2" ||
+          album["@attr"].rank === "3"
+            ? "flex justify-center items-center"
+            : "hidden")
+        }
+      >
+        {album["@attr"].rank === "1" ? (
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/179/179249.png"
+            alt="First Place"
+            className="w-full h-full"
+          />
+        ) : album["@attr"].rank === "2" ? (
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/5406/5406816.png"
+            alt="Second Place"
+            className="w-full h-full"
+          />
+        ) : album["@attr"].rank === "3" ? (
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/5299/5299016.png"
+            alt="Third Place"
+            className="w-full h-full"
+          />
+        ) : null}
+      </div>
       <div
         className="w-full rounded-md bg-cover bg-center bg-no-repeat aspect-square"
         style={{
           backgroundImage: `url(${imageUrl})`,
         }}
       />
-        <CardHeader className="w-full h-full p-0">
+      <CardHeader className="w-full h-full p-0">
         <CardTitle className="truncate pt-2 leading-5">{album.name}</CardTitle>
         <CardDescription className="truncate">
           {album.artist.name}
